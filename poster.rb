1f102f1621ac985b7b9e23fca7fb219ab7a9a878
Lindsay Holmwood <auxesis+pp-zero@gmail.com>
2010-01-26 17:08:24 +1100

    write the json straight from the rack input

--- poster.rb
+++ poster.rb
@@ -10,8 +10,9 @@ post "/update" do
 end
 
 def log_request
-  filename = File.expand_path(File.join(File.dirname(__FILE__), 'log', Time.now.to_i.to_s + '.yaml'))
-  File.open(filename, 'w') do |f|
-    f << params.to_yaml
-  end
+  filename = File.expand_path(File.join(File.dirname(__FILE__), 'log', Time.now.to_i.to_s + '.json'))
+  json = request.env["rack.input"].read
+  File.open(filename, 'w') { |f|
+    f << json
+  }
 end
